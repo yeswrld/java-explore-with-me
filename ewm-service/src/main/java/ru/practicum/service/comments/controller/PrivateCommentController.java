fb54d7aaf2022e943bddde71cdc2648cd870f849
Damir Minigaliev <dminigaliev@gmail.com>
2025-05-02 17:38:43 +0500

    3 этап, добавлен функционал комментариев к эвентам, тесты в постмане,checkstyle

--- ewm-service/src/main/java/ru/practicum/service/comments/controller/PrivateCommentController.java
+++ ewm-service/src/main/java/ru/practicum/service/comments/controller/PrivateCommentController.java
@@ -1,7 +1,6 @@
 package ru.practicum.service.comments.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
